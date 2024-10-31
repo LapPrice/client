0da1a9972f0bf97bb72f2bce06e666a38324e1f6
sh0723 <pshpark12345@naver.com>
2024-10-31 16:44:41 +0900

    초기 세팅

--- src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>초기세팅완료</h1>
-    </div>
-  );
-}
-
-export default App;
---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div>초기세팅완료</div>;
+}
+
+export default App;
