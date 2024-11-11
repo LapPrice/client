b2d5f718219db49d15eb0513abdc517cf8c3cb28
seungjongyoo <seungjongyoo@soongsil.ac.kr>
2024-11-11 21:02:19 +0900

    첫 번째 커밋, DB랑 연계하는걸 목표로 짜긴함

---
+++ src/SelectLaptopPage/SelectLaptopPage.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import 
+import {useNavigate} from "react-router-dom";
+
+
+const SelectLaptopPage = () => {
+
+
+    return(
+        <div>
+            <h1 className="SelectLaptop-text">  
+                selected specification
+            </h1>
+        </div> 
+
+    )
+}
+
+export default SelectLaptopPage
